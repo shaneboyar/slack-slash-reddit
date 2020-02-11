43a1f4ed85c9c2cc0d969bf996b46d63c26977aa
Shane <shane.boyar@gmail.com>
2020-02-11 10:00:57 -0500

    prune

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import dotenv from "dotenv";
 dotenv.config();
-import { logger } from "./shared/Logger";
-import cookieParser from "cookie-parser";
 import express from "express";
 import BaseRouter from "./routes";
 
@@ -11,11 +9,10 @@ const app = express();
 // Add middleware/settings/routes to express.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use("/api", BaseRouter);
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => {
-  logger.info("Express server started on port: " + port);
+  console.info("Express server started on port: " + port);
 });
